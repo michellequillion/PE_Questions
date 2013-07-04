b213a7977a8b9333f517f1b6b57dd68a7299cd5e
michellequillion <michellequillion@gmail.com>
2013-07-03 20:27:20 -0400

    Add Question 3

---
+++ Question3/Question3/PrimeFactors.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime ( long number)
+{
+	if (number==2)
+		return true;
+	else if (number<2)
+		return false;
+	for (long i=2; i<number; i++)
+	{
+		if (number%i==0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main ()
+{
+	long num=0;
+
+	num=600851475143;
+	long biggest=0;
+	for (long i=2; i<=num; i++)
+	{
+		if (num%i==0)
+		{
+			int prime = isPrime(i);
+			if (prime==1)
+			{
+				if (i>biggest)
+					biggest=i;
+			}
+		}
+	}
+	cout << biggest;
+
+	return 0;
+}
